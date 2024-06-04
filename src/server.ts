cf111b75301d0b12be5bf0dc268013e413859d3f
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-04 09:35:17 -0300

    server created

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get("/teams", async (request, response) => {
+    response.type("application/json").code(200)
+
+    return [
+        {
+            id: 1,
+            name: "Team 1"
+        },
+        {
+            id: 2,
+            name: "Team 2"
+        }
+    ]
+});
+
+server.listen({ port: 3333 }, () => {
+    console.log("Server running on port 3333");
+});
